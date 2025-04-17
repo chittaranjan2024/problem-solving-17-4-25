37889d9be2fe528a50cc9e1747bd44d88314a30e
Chittaranjan <Chittaranjan@guvi.in>
2025-04-17 12:32:03 +0530

    initial commit

---
+++ Day-1-ProblemSolving/src/com/test/PrimeNumberCheck.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class PrimeNumberCheck {
+	
+	private static boolean isPrime(int num)
+	{
+		if(num<=1) return false;	
+		if(num==2) return true;
+		if(num%2==0) return false;
+		
+		for(int i=2;i<=Math.sqrt(num);i=i+2)  // 13   2-3
+		{
+			if(num%i==0)
+			{
+				return false;
+			}
+		}
+		
+		
+		return true;
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int num=scanner.nextInt();
+		
+		System.out.println(isPrime(num));
+		
+		
+
+	}
+
+}
